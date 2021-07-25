ad466d56127c502e833996969834eea35e50729f
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-07-24 21:52:06 -0400

    add logic to save message with readStatus based on the receiver having activ conversation or not

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -5,7 +5,6 @@ import { withStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import { postMessage, getRecipientData } from "../../store/utils/thunkCreators";
 import socket from "../../socket";
-import axios from "axios";
 
 const styles = {
   root: {
@@ -39,42 +38,35 @@ function Input(props) {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    // Emitting message to check if the receivers active conversation is this one
-    // first sending to server bin/www
-    // socket.emit("sending-message");
-    // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
-
+    const reqBody = {
+      text: event.target.text.value,
+      recipientId: props.otherUser.id,
+      conversationId: props.conversationId,
+      sender: props.conversationId ? null : props.user,
+    };
     // if other user is not online, save message as unread
     if (!otherUser.online) {
-      const reqBody = {
-        text: event.target.text.value,
-        recipientId: props.otherUser.id,
-        conversationId: props.conversationId,
-        sender: props.conversationId ? null : props.user,
-        receiverHasRead: false,
-      };
-      await props.postMessage(reqBody);
+      let body = { ...reqBody, receiverHasRead: false };
+      await props.postMessage(body);
       setText("");
     } else {
-      // Have to try to implement current conv in otheruser section. when someone logout or login othyer user login status gets updated for eveyone
-      // Have to try to do that when an user changes his active chat
-
       // Updating thr state with for other users activeChat
       // Noticed two bugs, during first conversation after searching  a user, typed message are sent to other online users, goes away after refresh
       // if a user reloads a page and dont have any active chat open, their past active chat remains(reset active chat on page load)
 
       // two condition here, if online and dont have active chat then receiverHasRead = false
       // else if active chat/conv == current convo receiverHasRead = true
-      console.log("Line 61----", props.otherUser.id);
       await props.getRecipientData(props.conversationId, props.otherUser.id);
-      const reqBody = {
-        text: event.target.text.value,
-        recipientId: props.otherUser.id,
-        conversationId: props.conversationId,
-        sender: props.conversationId ? null : props.user,
-        receiverHasRead: false,
-      };
-      await props.postMessage(reqBody);
+      //if the reciver is in  the same conversation meaning message will be read
+      console.log(props.otherUser);
+      console.log(props.conversationId);
+      if (props.otherUser.activeConvo === props.conversationId) {
+        let body = { ...reqBody, receiverHasRead: true };
+        await props.postMessage(body);
+      } else {
+        let body = { ...reqBody, receiverHasRead: false };
+        await props.postMessage(body);
+      }
     }
   };
 
