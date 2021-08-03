ec5f5a7f005e91bda2a6b9ad546dfc88c1f2a589
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-08-02 23:38:51 -0400

    change structure of sign up page, add css

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
 import {
   Grid,
   Box,
@@ -11,11 +14,56 @@ import {
   FormHelperText,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
+import img from "./images/bg-img.png";
+import AuthHeader from "./AuthHeader";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexGrow: 8,
+  },
+  authWrapper: {
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    flexDirection: "column",
+    minHeight: "100vh",
+    paddingTop: 23,
+  },
+  introImage: {
+    objectFit: "cover",
+    height: "100%",
+    width: "100%",
+  },
+  welcome: {
+    fontSize: 26,
+    paddingBottom: 20,
+    color: "#000000",
+    fontWeight: 700,
+    fontFamily: "'Open Sans'",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    display: "flex",
+    flexDirection: "column",
+  },
+  submit: {
+    padding: 10,
+    width: 160,
+    borderRadius: theme.shape.borderRadius,
+    marginTop: 49,
+    fontSize: 16,
+    backgroundColor: "#3a8dff",
+    fontWeight: "bold",
+  },
+}));
 
 const Login = (props) => {
   const history = useHistory();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
+  const classes = useStyles();
 
   const handleRegister = async (event) => {
     event.preventDefault();
@@ -37,26 +85,41 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
-        </Grid>
-        <form onSubmit={handleRegister}>
-          <Grid>
-            <Grid>
-              <FormControl>
+    <Grid container component="main" className={classes.root}>
+      <Grid item xs={12} sm={5} md={6} elevation={6} component={Paper} square>
+        <img src={img} alt="" className={classes.introImage} />
+      </Grid>
+      <CssBaseline />
+      <Grid item xs={12} sm={7} md={6} elevation={6} component={Paper} square>
+        <Box className={classes.authWrapper}>
+          <AuthHeader
+            linkTo="/login"
+            asideText="Have an account?"
+            btnText="Login"
+          />
+          <Box width="100%" maxWidth={450} p={3} alignSelf="center">
+            <Grid container>
+              <Grid item xs>
+                <Typography
+                  className={classes.welcome}
+                  component="h1"
+                  variant="h5"
+                >
+                  Create an account!
+                </Typography>
+              </Grid>
+            </Grid>
+            <form onSubmit={handleRegister} className={classes.form}>
+              <FormControl margin="normal" required>
                 <TextField
                   aria-label="username"
                   label="Username"
+                  fullWidth
                   name="username"
                   type="text"
                   required
                 />
               </FormControl>
-            </Grid>
-            <Grid>
               <FormControl>
                 <TextField
                   label="E-mail address"
@@ -66,8 +129,6 @@ const Login = (props) => {
                   required
                 />
               </FormControl>
-            </Grid>
-            <Grid>
               <FormControl error={!!formErrorMessage.confirmPassword}>
                 <TextField
                   aria-label="password"
@@ -81,8 +142,6 @@ const Login = (props) => {
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
-            </Grid>
-            <Grid>
               <FormControl error={!!formErrorMessage.confirmPassword}>
                 <TextField
                   label="Confirm Password"
@@ -96,13 +155,19 @@ const Login = (props) => {
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
-            </Grid>
-            <Button type="submit" variant="contained" size="large">
-              Create
-            </Button>
-          </Grid>
-        </form>
-      </Box>
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                className={classes.submit}
+              >
+                Register
+              </Button>
+            </form>
+          </Box>
+          <Box p={1} alignSelf="center" />
+        </Box>
+      </Grid>
     </Grid>
   );
 };
